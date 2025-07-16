93aa51d36c1c94fad6eee8af6ca870138be3cbeb
Rathod Prakash <rathodprakash7926@gmail.com>
2025-07-16 21:03:38 +0530

    refactor: extract validation from addSweet() into a separate method

--- src/sweetshop.js
+++ src/sweetshop.js
@@ -3,7 +3,18 @@ class SweetShop{
         this.sweets = [];
     }
     addSweet(sweet){
-        this.sweets.push(sweet);
+        const { id, name, category, price, quantity } = sweet;
+
+        // valid input 
+        if( !id || !name || !category || price<0 || quantity<0){
+            throw new Error("Inavalid sweet Data");   
+        }
+        // check unique ID
+        const exists = this.sweets.find(s => s.id == id);
+        if (exists) {
+        throw new Error("Sweet with this ID already exists.");
+        }
+        this.sweets.push({ id, name, category, price, quantity });
     }
     getAllSweets(){
         return this.sweets
